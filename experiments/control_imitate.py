49ff687f2c52c099f760991dfae1e067e1c928fb
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-05-23 00:41:14 -0400

    Updated imitation learning model

--- experiments/control_imitate.py
+++ experiments/control_imitate.py
@@ -54,10 +54,7 @@ while True:
     vels = vels.squeeze().data.cpu().numpy()
     print(vels)
 
-    vels[1] *= 0.85
-
-    #vels = vels.clip(0, 1)
-    #vels *= 0.8
+    #vels[1] *= 0.85
 
     obs, reward, done, info = env.step(vels)
     #print('stepCount = %s, reward=%.3f' % (env.stepCount, reward))
