a6ca760f578646bd5d3a858a41be49a239832b01
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-06-01 16:51:26 -0400

    Added test

--- run_tests.py
+++ run_tests.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
+import os
 import numpy as np
 import gym
 import gym_duckietown
+from gym_duckietown.envs import SimpleSimEnv
 
 env = gym.make('SimpleSim-v0')
 
-first_obs = env.reset()
-
 # Check that the human rendering resembles the agent's view
+first_obs = env.reset()
 first_render = env.render('rgb_array')
 m0 = first_obs.mean()
 m1 = first_render.mean()
 assert m0 > 0 and m0 < 255
 assert abs(m0 - m1) < 5
 
+# Check that stepping works
 obs, _, _, _ = env.step(np.array([0.1, 0.1]))
+
+# Try loading each of the available map files
+for map_file in os.listdir('gym_duckietown/maps'):
+    map_name = map_file.split('.')[0]
+    env = SimpleSimEnv(map_name=map_name)
