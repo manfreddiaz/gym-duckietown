9527d809482e8475fe4daad9c82afe35e3796a2e
Manfred Diaz <takeitallsource@gmail.com>
2018-09-08 11:23:52 -0400

    [ADDED] Per episode text analysis.

--- imitation/training/_analysis.py
+++ imitation/training/_analysis.py
@@ -56,17 +56,18 @@ def algorithm_and_parametrization_by_optimization(config):
 def stats_summaries(summaries):
     for iteration_summary in summaries:
         print('method: {}'.format(iteration_summary.label))
-        print('\t reward: {}'.format(sum(iteration_summary.reward_history())))
-        # print('\t \t per episode: \r\n{}'.format(iteration_summary.reward_history()))
-        print('\t queries: {}'.format(sum(iteration_summary.queries_history())))
+        print('  reward: {}'.format(sum(iteration_summary.reward_history())))
+        # print('  per episode:')
+        # print('     {}'.format(iteration_summary.reward_history()))
+        print('  queries: {}'.format(sum(iteration_summary.queries_history())))
         # print('\t \t per episode: \r\n{}'.format(iteration_summary.queries_history()))
-        print('\t penalties: {}'.format(sum(iteration_summary.penalties_history())))
+        print('  penalties: {}'.format(sum(iteration_summary.penalties_history())))
         # print('\t \t per episode: \r\n{}'.format(iteration_summary.penalties_history()))
-        print('\t out bounds: {}'.format(sum(iteration_summary.out_bounds_history())))
+        print('  out bounds: {}'.format(sum(iteration_summary.out_bounds_history())))
         # print('\t \t per episode: \r\n{}'.format(iteration_summary.out_bounds_history()))
-        print('\t delta v: {}'.format(sum(iteration_summary.delta_v_history())))
+        print('  delta v: {}'.format(sum(iteration_summary.delta_v_history())))
         # print('\t \t per episode: \r\n{}'.format(iteration_summary.delta_v_history()))
-        print('\t delta theta: {}'.format(sum(iteration_summary.delta_theta_history())))
+        print('  delta theta: {}'.format(sum(iteration_summary.delta_theta_history())))
         # print('\t \t per episode: \r\n{}'.format(iteration_summary.delta_theta_history()))
 
 def render_summaries(summaries):
