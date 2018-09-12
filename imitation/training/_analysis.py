0580306b1268ad6d43a0407dcf0b4dd52a220466
Manfred Diaz <takeitallsource@gmail.com>
2018-09-12 16:42:47 -0400

    [FIXED] Convert penalties in safety violations.

--- imitation/training/_analysis.py
+++ imitation/training/_analysis.py
@@ -58,21 +58,21 @@ def algorithm_and_parametrization_by_optimization(config):
 def stats_summaries(summaries):
     for iteration_summary in summaries:
         print('method: {}'.format(iteration_summary.label))
-        print('  reward: {}'.format(iteration_summary.reward()))
-        # print('  per episode:')
-        # print('     {}'.format(iteration_summary.reward_history()))
+        print('\treward: {}'.format(iteration_summary.reward()))
+        print('\t\t  per episode:')
+        print('     {}'.format(iteration_summary.reward_history()))
         print('  queries: {}'.format(iteration_summary.queries()))
-        # print('\t \t per episode: \r\n{}'.format(iteration_summary.queries_history()))
+        print('\t\t per episode: \r\n{}'.format(iteration_summary.queries_history()))
         print('  penalties: {}'.format(iteration_summary.penalties()))
-        # print('\t \t per episode: \r\n{}'.format(iteration_summary.penalties_history()))
+        print('\t\t per episode: \r\n{}'.format(iteration_summary.penalties_history()))
         print('  out bounds: {}'.format(iteration_summary.out_bounds()))
-        # print('\t \t per episode: \r\n{}'.format(iteration_summary.out_bounds_history()))
+        print('\t\t per episode: \r\n{}'.format(iteration_summary.out_bounds_history()))
         print('  delta v_l: {}'.format(iteration_summary.delta_v_l()))
-        # print('\t \t per episode: \r\n{}'.format(iteration_summary.delta_v_history()))
+        print('\t \t per episode: \r\n{}'.format(iteration_summary.delta_v_history()))
         print('  delta v_r: {}'.format(iteration_summary.delta_v_r()))
-        # print('\t \t per episode: \r\n{}'.format(iteration_summary.delta_theta_history()))
+        print('\t \t per episode: \r\n{}'.format(iteration_summary.delta_theta_history()))
         print('  no_control: {}'.format(iteration_summary.no_control()))
-        # print('\t \t per episode: \r\n{}'.format(iteration_summary.delta_theta_history()))
+        print('\t \t per episode: \r\n{}'.format(iteration_summary.delta_theta_history()))
 
 
 def render_summaries(summaries):
